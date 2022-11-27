0adf05b8a471b0c7b3bc9e1046f54dfeca532c74
Med-lemineHmd <leminlemin00@gmail.com>
2022-11-28 00:38:32 +0100

    Create About Section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { Footer, Navbar } from '../components';
+import { Footer, Navbar } from "../components";
 import {
   About,
   Explore,
@@ -8,19 +8,28 @@ import {
   Insights,
   WhatsNew,
   World,
-} from '../sections';
+} from "../sections";
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div>
     <World />
-    <Insights />
-    <Feedback />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
@@ -16,7 +16,7 @@ const Hero = () => (
     >
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          Metavers
+          Metaverse
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
@@ -24,19 +24,22 @@ const Hero = () => (
         >
           <h1 className={styles.heroHeading}>Ma</h1>
           <div className={styles.heroDText} />
-          <h1 className={styles.heroHeading}>ness</h1>
+          <h1 className={styles.heroHeading}>Ness</h1>
         </motion.div>
       </div>
+
       <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
+        variants={slideIn('right', 'tween', 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]" />
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+
         <img
           src="/cover.png"
-          alt="cover"
-          className="w-full sm:h-[500px] h-[300px] object-cover rounded-tl-[140px] z-10 relative"
+          alt="hero_cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
         />
+
         <a href="#explore">
           <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
             <img
