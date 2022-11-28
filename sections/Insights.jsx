b5e9a1165d86faa146e622809fa68475aaaf6270
Med-lemineHmd <leminlemin00@gmail.com>
2022-11-29 00:23:36 +0100

    Create Feddback Section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { InsightCard, TypingText, TitleText } from "../components";
-import { insights } from "../constants";
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { InsightCard, TypingText, TitleText } from '../components';
+import { insights } from '../constants';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-0`}>
